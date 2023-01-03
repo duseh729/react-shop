23c14fd5685fe1eb94028470a92ce78fec66e340
DoYeon Ahn <duseh729@naver.com>
2023-01-03 16:19:55 +0900

    컴포넌트화 map 데이터바인딩

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 // Bootstrap 사용을 위한 import
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import data from "./data";
 
 function App() {
+  let [shoes, setShoes] = useState(data);
   return (
     <div className="App">
       {/* 참고로 className으로 커스터마이징이 가능하다. */}
@@ -20,23 +23,21 @@ function App() {
       {/* bootstrap으로 비율 조절후 사진은 웹에서 가져옴 */}
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
+          {shoes.map((shoes, i) => {
+            return <Shoes i={i} shoes={shoes} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Shoes(props) {
+  return (
+    <div className="col-md-4" key={props.i}>
+      <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
