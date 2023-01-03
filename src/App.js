8c4decd6be98562fc7fde34eb0e6d649ce7c45e9
DoYeon Ahn <duseh729@naver.com>
2023-01-03 15:31:11 +0900

    bootstrap과 상품이미지 넣기

--- src/App.js
+++ src/App.js
@@ -12,11 +12,31 @@ function App() {
           <Navbar.Brand href="#home">DYshop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      <div className="main-bg"></div>
+      {/* bootstrap으로 비율 조절후 사진은 웹에서 가져옴 */}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
