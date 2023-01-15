fcf67b19787153d273149b926b07689eb9537301
Doyeon <duseh729@naver.com>
2023-01-15 23:08:22 +0900

    react-query 세팅

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import { BrowserRouter } from "react-router-dom";
 // npm install @reduxjs/toolkit react-redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <QueryClientProvider client={queyClient}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </QueryClientProvider>
   // </React.StrictMode>
 );
 
